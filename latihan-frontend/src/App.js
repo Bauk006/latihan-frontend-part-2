0d66c94e44d8b56f53639d5bae1281310cc41032
Evan <evanmarhian2@gmail.com>
2025-11-05 10:04:38 +0700

    Menambahkan konfigurasi Routing pada file App.js

---
+++ latihan-frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<h3 className="text-center mt-5">404 Not Found</h3>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
